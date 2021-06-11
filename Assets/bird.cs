66522c3d45a8d143af36199398de1dad8f57e4e6
Kamal Johnson <kamaljohnson12345@gmail.com>
2021-06-12 03:50:44 +0530

    :art: Add bird flap

---
+++ Assets/bird.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    public bool isPlaying;
+    
+    public float flapForce;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        
+        Move();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Flap();
+        }
+    }
+
+    void Move()
+    {
+            rb.isKinematic = false;
+    }
+
+    void Stop()
+    {
+            rb.isKinematic = true;
+    }
+
+    void Flap()
+    {
+        rb.velocity = Vector2.up * flapForce;
+    }    
+}
